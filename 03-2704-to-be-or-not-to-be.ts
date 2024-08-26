f563bb502f0ec051c1368cbaa0876e2a3fa48450
yuki16bit <yuki16bit@gmail.com>
2024-08-27 00:58:20 +0800

    feat: completed the 3rd day challenge

---
+++ 03-2704-to-be-or-not-to-be.ts
@@ -0,0 +1,20 @@
+// * https://leetcode.com/problems/to-be-or-not-to-be/description/?envType=study-plan-v2&envId=30-days-of-javascript
+// * Closure
+
+type ToBeOrNotToBe = {
+  toBe: (val: any) => boolean;
+  notToBe: (val: any) => boolean;
+};
+
+function expect(val: any): ToBeOrNotToBe {
+  return {
+    toBe: (testCase) => {
+      if (testCase === val) return true;
+      throw new Error("Not Equal");
+    },
+    notToBe: (testCase) => {
+      if (testCase !== val) return true;
+      throw new Error("Equal");
+    }
+  }
+};
