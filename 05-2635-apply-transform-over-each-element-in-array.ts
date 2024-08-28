e8b2b6743cb3c440a814fd4b79e57274ad08abdc
yuki16bit <yuki16bit@gmail.com>
2024-08-28 22:14:44 +0800

    feat: completed the 5th day challenge

---
+++ 05-2635-apply-transform-over-each-element-in-array.ts
@@ -0,0 +1,10 @@
+// * https://leetcode.com/problems/apply-transform-over-each-element-in-array/description/?envType=study-plan-v2&envId=30-days-of-javascript
+// * Basic Array Transformations
+
+function map(arr: number[], fn: (n: number, i: number) => number): number[] {
+  const newArr: number[] = [];
+  for (let i = 0; i < arr.length; i+=1) {
+      newArr.push(fn(arr[i], i));
+  };
+  return newArr;
+};
